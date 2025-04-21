05002f02f739eeec4725aea7199473d98a4be950
Ronin021 <lavro.sandor@gmail.com>
2025-04-21 21:15:39 +0200

    tablazat oop

--- oop/area.js
+++ oop/area.js
@@ -1,6 +1,18 @@
 
 class Area{ 
 /**
+ * @param {HTMLDivElement}
+ */
+#div; // privát változó, ami tárolja a div elemet
+
+/**
+ * @returns {HTMLDivElement}
+ */
+get div(){ // getter, ami visszaadja a div elemet
+    return this.#div; // visszaadja a privát változót
+}
+
+/**
  * 
  * @param {string} ClassName - class név, amit a div elemhez szeretnénk rendelni 
  */
@@ -12,8 +24,38 @@ constructor(ClassName){ // konstruktor, ami létrehozza az Area objektumot a meg
         document.body.appendChild(containerDiv); // a body-hoz hozzáadja a létrehozott div elemet
     }
 
-    const div = document.createElement('div'); // létrehoz egy új div elemet
-    div.className = ClassName; // beállítja a class nevét
-    containerDiv.appendChild(div); // a containerDiv-hez hozzáadja a létrehozott div elemet
+    this.#div = document.createElement('div'); // létrehoz egy új div elemet
+    this.#div.className = ClassName; // beállítja a class nevét
+    containerDiv.appendChild(this.#div); // a containerDiv-hez hozzáadja a létrehozott div elemet    
 }
+
+
+}
+
+class Table extends Area{ // a Table osztály, ami öröklődik az Area osztályból
+/**
+ * 
+ * @param {string} CssClass - class név, amit a table elemhez szeretnénk rendelni 
+ */
+    constructor(CssClass){ // konstruktor, ami létrehozza a Table objektumot a megadott class névvel
+        super(CssClass); // meghívja az Area osztály konstruktorát
+
+        const table = document.createElement('table'); // létrehoz egy új table elemet
+        this.div.appendChild(table); // a div-hez hozzáadja a létrehozott table elemet
+
+        const tableHeader = document.createElement('thead'); // létrehoz egy új thead elemet
+        table.appendChild(tableHeader); // a table-hez hozzáadja a létrehozott thead elemet
+
+        const tableHeaderRow = document.createElement('tr'); // létrehoz egy új tr elemet
+        tableHeader.appendChild(tableHeaderRow); // a tableHeader-hez hozzáadja a létrehozott tr elemet
+
+        const fejlecekneve = ['Szerző', 'Műfaj', 'Cím']; // a táblázat fejlécének nevei
+        for (const fejlec of fejlecekneve) { // végigmegy a fejlecek tömbön
+            const th = document.createElement('th'); // létrehoz egy új th elemet
+            th.innerText = fejlec; // beállítja a th elem szövegét
+            tableHeaderRow.appendChild(th); // a tableHeaderRow-hoz hozzáadja a létrehozott th elemet
+        }
+        const tableBody = document.createElement('tbody'); // létrehoz egy új tbody elemet
+        table.appendChild(tableBody); // a table-hez hozzáadja a létrehozott tbody elemet
+    }
 }
